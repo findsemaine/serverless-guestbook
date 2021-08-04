fbe142137d32294bad2b94a9233fc283f21d0938
findsemaine <marcantoine.comte@gmail.com>
2021-08-04 13:53:48 +0000

    first commit

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://16fc2531.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
